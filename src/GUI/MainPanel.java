8385d1f3434d00f65694d59a35f68b7d51632dc5
rdurs <rdurso@outlook.fr>
2021-01-29 14:46:31 +0100

    Préparation a la création de l'IHM

--- src/GUI/MainPanel.java
+++ src/GUI/MainPanel.java
@@ -1,5 +1,29 @@
 package GUI;
 
-public class MainPanel {
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+import config.GuiData;
+
+
+
+public class MainPanel extends JFrame implements Runnable {
+	
+	private final static Dimension preferredSize = new Dimension(GuiData.WINDOW_WIDTH,GuiData.WINDOW_HEIGHT);
+	public MainPanel (String title) {
+		super(title);
+		init();
+	}
+
+	private void init() {
+		
+	}
+	
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
---
+++ src/Test/TestGUI.java
@@ -0,0 +1,14 @@
+package Test;
+
+import javax.swing.JFrame;
+
+import GUI.MainPanel;
+
+public class TestGUI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame testGui =	new MainPanel("Dronemation");
+	}
+
+}
