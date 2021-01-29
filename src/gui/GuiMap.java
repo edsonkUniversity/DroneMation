870f0df12966ea3836c794525c0b1b5993ea58e0
edsonDeCavalho <76922937+edsonDeCavalho@users.noreply.github.com>
2021-01-29 17:08:41 +0100

    Edson

--- src/gui/GuiMap.java
+++ src/gui/GuiMap.java
@@ -1,16 +1,32 @@
 package gui;
 
 import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 import data.Map;
 import process.ElementManager;
 
 public class GuiMap extends JPanel {
-	Map map;
-	ElementManager manager;
-	public GuiMap(Map map, ElementManager manager) {
-		this.map=map;
-		this.manager=manager;
+    
+    private BufferedImage image;
+    
+    private Map map;
+    private ElementManager manager;
+    
+    
+    public GuiMap(Map map, ElementManager manager) {
+        this.map=map;
+        this.manager=manager;
 
-	}
+    }
+    
+
+    
+    public void paintMap(Graphics g) {
+        paintComponent(g);
+        //Coordonnées à compléter
+        g.drawImage(image, 0, 0, this);
+    }
 }
